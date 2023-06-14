fedf526c2edc5396d75f7eb4c385d17a72bf5708
Sergio Miracco <sergio.miracco@triamec.com>
2023-06-14 12:06:16 +0200

    Barcode example proto untested

--- Barcode/TamaProgram.cs
+++ Barcode/TamaProgram.cs
@@ -1,20 +1,15 @@
-﻿// The number of these two  directives must match the device's Tama Virtual Machine ID and Register Layout ID,
-// respectively.
-using System.Reflection;
-using Triamec.Tama.Rlid19;
+﻿using Triamec.Tama.Rlid19;
 using Triamec.Tama.Vmid5;
 using Triamec.TriaLink;
-
-[assembly: AssemblyVersion("2.0.0.0")]
 
 // The class name (as well as the project name and the namespace) contributes to the file name of the produced Tama
 // program. This file is located in the bin\Debug or bin\Release subfolders and will commonly be copied into the
 // Tama directory of the default workspace, too.
 [Tama]
-internal static class TamaProgram
+internal class TamaProgram
 {
 
-    // Linear State Machine
+    // Linear State Machine, shown in Register.Application.TamaControl.IsochronousMainState
     private enum State
     {
         Idle,
@@ -25,41 +20,62 @@ internal static class TamaProgram
         MoveRowNegative
     }
 
+    // User commands, invoked at Register.Application.TamaControl.IsochronousMainCommand
     private static class Command
     {
         public const int None = 0;
         public const int Go = 1;
     }
 
-    // Static variables can be used, but are not shared between the AsynchronousMain and the other tasks (Imagine a
-    // [ThreadStatic] attribute here). For sharing, use general purpose registers.
-    static int _rows;
-    static int _row_index;
-    static readonly uint[] cPulseCountPositive = { 8, 7, 8, 7, 5};
-    static readonly uint[] cPulseCountNegative = { 8, 7, 8, 7, 5};
-    static readonly int[] cPulseOn = { 1, 0, 1, 0, 1 };
-
+    // pulsing application constants
+    const int cRows = 2;
+    const float cMoveStartPosition = 19f;
+    const float cMoveEndPosition = 24.3f;
     const float cReferencePositive = 20f;
     const float cReferenceNegative = 23.3f;
     const float cDeltaPosition = 0.1f;
     const float cPulseWidth = 0.00001f;
 
-    // Choose how to run the program. Additional entry points for other tasks can be specified in this same program.
+    // internal variables
+    static int _row_index;
+    static int[] cPulseCountPositive = new int[5];
+    static int[] cPulseCountNegative = new int[5];
+    static int[] cPulseOn = new int[5];
+
+    // Constructor
+    public TamaProgram()
+    {
+        cPulseCountPositive[0] = 8;
+        cPulseCountPositive[1] = 5;
+        cPulseCountPositive[2] = 8;
+        cPulseCountPositive[3] = 5;
+        cPulseCountPositive[4] = 5;
+
+        cPulseCountNegative[0] = 5;
+        cPulseCountNegative[1] = 5;
+        cPulseCountNegative[2] = 8;
+        cPulseCountNegative[3] = 5;
+        cPulseCountNegative[4] = 8;
+
+        cPulseOn[0] = 1;
+        cPulseOn[1] = 0;
+        cPulseOn[2] = 1;
+        cPulseOn[3] = 0;
+        cPulseOn[4] = 1;
+    }
+
+    // -- entry point --
     [TamaTask(Task.IsochronousMain)]
     static void Main()
     {
-
-        // Template state machine showing the picture of how a task is structured and which registers are commonly used
-        // for status/control operations.
-        switch ((State) Register.Application.TamaControl.IsochronousMainState)
+        switch ((State)Register.Application.TamaControl.IsochronousMainState)
         {
             case State.Idle:
                 switch (Register.Application.TamaControl.IsochronousMainCommand)
                 {
                     case Command.Go:
-                        _rows = Register.Application.Parameters.Integers[0];
                         _row_index = 0;
-                        MoveTo(0);
+                        MoveTo(cMoveStartPosition);
                         Register.Application.TamaControl.IsochronousMainState = (int)State.MoveToStart;
                         break;
                 }
@@ -67,31 +83,35 @@ internal static class TamaProgram
             case State.MoveToStart:
                 if (Register.Axes_0.Signals.PathPlanner.Done)
                 {
-                    if (_rows > 0)
-                    {
-                        Register.Axes_0.Commands.OptionModule.PU_Mode = OptionPuMode.Disabled;
-                        Register.Application.TamaControl.IsochronousMainState = (int)State.FillFifoPositive;
-                    } else
-                    {
-                        Register.Application.TamaControl.IsochronousMainState = (int)State.Idle;
-                    }
+                    Register.Axes_0.Commands.OptionModule.PU_Mode = OptionPuMode.Disabled;
+                    Register.Application.TamaControl.IsochronousMainState = (int)State.FillFifoPositive;
                 }
                 break;
             case State.FillFifoPositive:
-                if (_row_index < _rows)
+                // set row reference position before activating the mode
+                if (Register.Axes_0.Commands.OptionModule.PU_ReferencePosition != cReferencePositive)
                 {
-                    uint segment_index = Register.Axes_0.Signals.OptionModule.PU_ActualPulseCount;
+                    Register.Axes_0.Commands.OptionModule.PU_ReferencePosition = cReferencePositive;
+                    break;
+                }
+                if (Register.Axes_0.Commands.OptionModule.PU_Mode != OptionPuMode.Fifo)
+                {
+                    Register.Axes_0.Commands.OptionModule.PU_Mode = OptionPuMode.Fifo;
+                    break;
+                }
+                if (_row_index < cRows)
+                {
+                    int segment_index = 512 - (int)Register.Axes_0.Signals.OptionModule.PU_FreeFifoEntries;
                     if (segment_index < cPulseCountPositive.Length)
                     {
-                        Register.Axes_0.Commands.OptionModule.PU_ReferencePosition = cReferencePositive;
                         Register.Axes_0.Commands.OptionModule.PU_DeltaPosition = cDeltaPosition;
                         Register.Axes_0.Commands.OptionModule.PU_PulseWidth = cPulseWidth * cPulseOn[segment_index];
-                        Register.Axes_0.Commands.OptionModule.PU_Count = cPulseCountPositive[segment_index];
+                        Register.Axes_0.Commands.OptionModule.PU_Count = (uint) cPulseCountPositive[segment_index];
                         Register.Axes_0.Commands.OptionModule.PU_Fifo = OptionPuFifo.Append;
                     }
                     else
                     {
-                        MoveTo(24);
+                        MoveTo(cMoveEndPosition);
                         Register.Application.TamaControl.IsochronousMainState = (int)State.MoveRowPositive;
                     }
                 }
@@ -103,35 +123,39 @@ internal static class TamaProgram
             case State.MoveRowPositive:
                 if (Register.Axes_0.Signals.PathPlanner.Done)
                 {
-                    if (Register.Axes_0.Signals.OptionModule.PU_ActualPulseCount != 0)
-                    {
-                        // abort and indicate an error with Booleans[0]
-                        Register.Application.TamaControl.IsochronousMainState = (int)State.Idle;
-                        Register.Application.Variables.Booleans[0] = true;
-                        break;
-                    }
                     _row_index++;
+                    Register.Axes_0.Commands.OptionModule.PU_Mode = OptionPuMode.Disabled;
                     Register.Application.TamaControl.IsochronousMainState = (int)State.FillFifoNegative;
-                    // Move other Axis
+
+                    // -- Move other Axis to align with next row --
                 }
                 break;
             case State.FillFifoNegative:
-                if (_row_index < _rows)
+                // set row reference position before activating the mode
+                if (Register.Axes_0.Commands.OptionModule.PU_ReferencePosition != cReferencePositive)
                 {
-                    uint segment_index = Register.Axes_0.Signals.OptionModule.PU_ActualPulseCount;
+                    Register.Axes_0.Commands.OptionModule.PU_ReferencePosition = cReferenceNegative;
+                    break;
+                }
+                if (Register.Axes_0.Commands.OptionModule.PU_Mode != OptionPuMode.Fifo)
+                {
+                    Register.Axes_0.Commands.OptionModule.PU_Mode = OptionPuMode.Fifo;
+                    break;
+                }
+                if (_row_index < cRows)
+                {
+                    int segment_index = 512 - (int)Register.Axes_0.Signals.OptionModule.PU_FreeFifoEntries;
                     if (segment_index < cPulseCountPositive.Length)
                     {
-                        Register.Axes_0.Commands.OptionModule.PU_ReferencePosition = cReferenceNegative;
                         Register.Axes_0.Commands.OptionModule.PU_DeltaPosition = -cDeltaPosition;
                         Register.Axes_0.Commands.OptionModule.PU_PulseWidth = cPulseWidth * cPulseOn[segment_index];
-                        Register.Axes_0.Commands.OptionModule.PU_Count = cPulseCountNegative[segment_index];
+                        Register.Axes_0.Commands.OptionModule.PU_Count = (uint) cPulseCountNegative[segment_index];
                         Register.Axes_0.Commands.OptionModule.PU_Fifo = OptionPuFifo.Append;
                     }
                     else
                     {
-                        MoveTo(20);
+                        MoveTo(cMoveStartPosition);
                         Register.Application.TamaControl.IsochronousMainState = (int)State.MoveRowNegative;
-                        _row_index++;
                     }
                 }
                 else
@@ -142,19 +166,13 @@ internal static class TamaProgram
             case State.MoveRowNegative:
                 if (Register.Axes_0.Signals.PathPlanner.Done)
                 {
-                    if (Register.Axes_0.Signals.OptionModule.PU_ActualPulseCount != 0)
-                    {
-                        // abort and indicate an error with Booleans[0]
-                        Register.Application.TamaControl.IsochronousMainState = (int)State.Idle;
-                        Register.Application.Variables.Booleans[0] = true;
-                        break;
-                    }
                     _row_index++;
+                    Register.Axes_0.Commands.OptionModule.PU_Mode = OptionPuMode.Disabled;
                     Register.Application.TamaControl.IsochronousMainState = (int)State.FillFifoPositive;
-                    // Move other Axis
+
+                    // -- Move other Axis to align with next row --
                 }
                 break;
-
         }
     }
 
